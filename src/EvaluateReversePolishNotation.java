741d0fad7f065175de038e0e2711fd3429945763
OP-75 <hparyani75@gmail.com>
2023-09-01 13:36:48 +0530

    .

--- src/EvaluateReversePolishNotation.java
+++ src/EvaluateReversePolishNotation.java
@@ -1,47 +1,63 @@
-import java.util.Stack;
+import java.util.*;
+import java.lang.*;
 
-//THIS CODE IS STILL INCOMPLETE & YET TO BE SUBMITTED ON LEETCODE
+
+//IMPORTANT!!!! While comparing 2 strings dont use "==" use str1.equals(str2) functions, since "==" compares the 2 string addresses which can or cannot be the same!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 
 class Solution {
 
-    private int solve(int num1, int num2, String operator) {
+    int solve(int num1, int num2, String operator){
 
-        if (operator == "*")
-            return num1 * num2;
-        else if (operator == "/")
-            return num1 / num2;
-        else if (operator == "-")
-            return num1 - num2;
-        else
-            return num1 + num2;
+        if(operator.equals("*")) return num1*num2;
+        else if(operator.equals("/")) return num1/num2;
+        else if(operator.equals("-")) return num1-num2;
+        else if(operator.equals("+")) return num1+num2;
+        else  return 0;
     }
 
-    private boolean isOperator(String operator) {
+    boolean isOperator(String operator){
 
-        if (operator == "*" || operator == "/" || operator == "-" || operator == "+")
-            return true;
+        // if (operator == "*" || operator == "/" || operator == "-" || operator == "+")
+        //     return true;
+        if(operator.equals("*") || operator.equals("/") || operator.equals("-") || operator.equals("+"))
+        return true;
 
         else
             return false;
     }
 
-    private int mySolution(String[] tokens) {
+
+
+
+
+    private int mySolution(String[] tokens){
 
         Stack<Integer> st = new Stack<>();
-
-        for (int i = 0; i < tokens.length; i++) {
+        
+    
+        for(int i=0; i<tokens.length; i++){
 
             String token = tokens[i];
-            if (isOperator(token)) {
+
+            if(isOperator(token)==true){
+                System.out.println("Inside if");
                 int num2 = st.pop();
                 int num1 = st.pop();
                 int result = solve(num1, num2, token);
+                System.out.println("res = "+result);
 
                 st.push(result);
-            } else {
-                int asNum = Integer.parseInt(token);
+            }
+            else{
+                int asNum = Integer.MIN_VALUE;
+                try{
+                asNum = Integer.parseInt(token);
                 st.push(asNum);
+                }
+                catch(Exception e){
+                    System.out.println(e);
+                }
             }
 
         }
@@ -60,7 +76,9 @@ public class EvaluateReversePolishNotation {
     public static void main(String[] args) {
         Solution o = new Solution();
 
-        String tokens[] = { "2", "1", "+", "3", "*" };
+        String tokens[] = {"2","1","+","3","*"};
+        tokens = new String[]{"4","13","5","/","+"};
+        tokens = new String[] {"10","6","9","3","+","-11","*","/","*","17","+","5","+"};
         System.out.println(o.evalRPN(tokens));
     }
 }
