8e799caa536d1dd73e4fd6020527440b4de87d8b
OP-75 <hparyani75@gmail.com>
2023-11-15 09:51:53 +0530

    .

---
+++ src/MaximumElementAfterDecreasingandRearranging.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class MaximumElementAfterDecreasingandRearranging {
+    public int maximumElementAfterDecrementingAndRearranging(int[] arr) {
+
+        Arrays.sort(arr);
+
+        arr[0] = 1;
+
+        if (arr.length==1) {
+            return arr[0];
+        }
+
+        
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i]-arr[i-1]<=1) {
+                continue;
+            }
+            else{
+                arr[i] = arr[i-1] + 1;
+            }
+        }
+
+        return arr[arr.length-1];
+
+    }
+
+}
